299a1d1d59ff674e8347bb4eed341d406ba90bec
Arturo Gonzalez <arturo@arturosolutions.com.au>
2024-06-11 16:37:47 +1000

    Updated README.md

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+"""
+This module initializes the Train Network and finds the shortest route from city A to city E using
+a given timetable and journey durations.
+"""
+
 import datetime
 import csv
-
 from src.logger import logger
 from src.journey import Journey
 from src.network import TrainNetwork
 
 
 def load_routes_from_csv(file_path):
+    """
+    Load routes from a CSV file and create Journey instances.
+    :param file_path: The path to the CSV file containing route information.
+    :return: A list of Journey instances.
+    """
     routes = []
-    with open(file_path, mode='r') as csvfile:
+    with open(file_path, mode='r', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             start_station = row['start_station'].strip()
@@ -26,7 +35,13 @@ def load_routes_from_csv(file_path):
 
 
 def run(file_path):
-    logger.info('*' * 10 + ' Initialising Train Network ' + '*' * 10)
+    """
+    Initialize the Train Network, build the network from the given routes, and find the shortest route
+    from city A to city E.
+    :param file_path: The path to the CSV file containing route information.
+    :return: None
+    """
+    logger.info('%s Initialising Train Network %s', '*' * 10, '*' * 10)
 
     routes = load_routes_from_csv(file_path)
 
@@ -37,13 +52,13 @@ def run(file_path):
     finish_time = datetime.datetime.now()
 
     if not quickest_route:
-        logger.info(f'No journey available from A to E in {file_path}, please check timetable is correct')
+        logger.info('No journey available from A to E in %s, please check timetable is correct', file_path)
     else:
-        logger.info(f'The quickest route in {file_path} is {quickest_route[0]}, duration {quickest_route[1]}')
+        logger.info('The quickest route in %s is %s, duration %s', file_path, quickest_route[0], quickest_route[1])
 
-    logger.info(f'Time taken for {file_path}: {finish_time - start_time}\n')
+    logger.info('Time taken for %s: %s', file_path, finish_time - start_time)
 
 
 if __name__ == '__main__':
-    file_path = 'data/routes_0.csv'  # File path to the specific CSV file
-    run(file_path)
+    FILE_PATH = 'data/routes_0.csv'  # File path to the specific CSV file
+    run(FILE_PATH)
--- src/strategies.py
+++ src/strategies.py
@@ -1,15 +1,38 @@
 """
-This module contains the constants used in the application.
+This module contains the strategy classes used for finding routes in the Train Network application.
 """
+
 from abc import ABC, abstractmethod
 
 
 class RouteStrategy(ABC):
+    """
+    Abstract base class for route finding strategies.
+    """
+
     @abstractmethod
     def find_route(self, train_network, start_station, end_station):
+        """
+        Find the route between start_station and end_station using a specific strategy.
+        :param train_network: The train network to find routes in.
+        :param start_station: The starting station.
+        :param end_station: The destination station.
+        :return: The route and its duration.
+        """
         pass
 
 
 class ShortestTimeStrategy(RouteStrategy):
+    """
+    Strategy for finding the shortest time route in the train network.
+    """
+
     def find_route(self, train_network, start_station, end_station):
+        """
+        Find the shortest time route between start_station and end_station.
+        :param train_network: The train network to find routes in.
+        :param start_station: The starting station.
+        :param end_station: The destination station.
+        :return: The route and its duration.
+        """
         return train_network.find_shortest_time_route(start_station, end_station)
